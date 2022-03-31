932f9cbadc53d19ce6ccef0ba90defdc435bd58e
Kory <KorySingleton35@gmail.com>
2022-03-31 19:28:00 -0400

    adding text-server

--- text-server.c
+++ text-server.c
@@ -10,14 +10,18 @@
 using namespace std;
 
 int main() {
+	//Step One occurs
 	puts("server started");
 	int shmid;
 	key_t key = ftok("shmfile", 65);
 	shmid = shmget(key, 1024, 0666|IPC_CREAT);
+	//Step two occurs below
 	char *str = (char*) shmat(shmid, (void*)0, 0);
 	puts("CLIENT REQUEST RECIEVED");
 	puts("\tMEMORY OPEN");
+	//Step three occurs
 	puts(str);
+	//Step four occurs below
 	printf("\tOPENING: %s\n", str);
 	FILE *in_file = fopen(str, "r");
 	if (!in_file) {
@@ -32,23 +36,14 @@ int main() {
 	str = (char*) malloc(sb.st_size);
 	char *file_contents =(char*) malloc(sb.st_size);
 	//int index = 0;
-	char *boo;
-	for (int a = 0; a < strlen(str); a++) 
-		boo[a] = str[a];
-	int length = strlen(boo);
-	for (int a = 0; a < strlen(boo); a++) {
-		if (str[0] = boo[a]) {
-			memmove(str, str+1, strlen(str));
-		}
-	}
 	printf("The value of str is: %s\n", str);
 	while (fgets(file_contents, sb.st_size, in_file)) {
 		strcat(str, file_contents);
 		//strcat(str, "\n");
 	}
 	fclose(in_file);
+	shmdt(str);
 	puts("\tFILE CLOSED");
-	puts(str);
 	puts("\tMEMORY CLOSED");
 	return 0;
 }
